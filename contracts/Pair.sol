fc2cac78516b03eb1cc60b4e5d83692d3f94e445
Kingsley Victor <javaprodigy56@gmail.com>
2025-01-17 16:48:39 +0100

    chore: update Pair contract

--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -6,9 +6,9 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "./interfaces/IPair.sol";
-import "./interfaces/IDibs.sol";
-import "./interfaces/IPairCallee.sol";
+import {IPair} from "./interfaces/IPair.sol";
+import {IDibs} from "./interfaces/IDibs.sol";
+import {IPairCallee} from "./interfaces/IPairCallee.sol";
 import {IPairFactory} from "./interfaces/IPairFactory.sol";
 import {PairFees} from "./PairFees.sol";
 
