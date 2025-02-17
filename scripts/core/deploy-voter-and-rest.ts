427ff8e36189e155c0beee60a5336f7003e56902
Kingsley Victor <javaprodigy56@gmail.com>
2025-02-17 22:34:58 +0100

    deploy to monad testnet

--- scripts/core/deploy-voter-and-rest.ts
+++ scripts/core/deploy-voter-and-rest.ts
@@ -1,5 +1,16 @@
 import hardhat from "hardhat";
-import { BribeFactory, MinterUpgradeable, PairHelper, Reactor, RewardHelper, RewardsDistributor, Router, TradeHelper, VeNFTHelper, Voter } from "../../artifacts/types";
+import {
+  BribeFactory,
+  MinterUpgradeable,
+  PairHelper,
+  Reactor,
+  RewardHelper,
+  RewardsDistributor,
+  Router,
+  TradeHelper,
+  VeNFTHelper,
+  Voter
+} from "../../artifacts/types";
 import { deploy, getContractWithAddress, writeOutput } from "../helpers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import path from "path";
@@ -33,49 +44,58 @@ async function main() {
   // Get deployments
   const deployments = readDeploymentsOutput(hardhat);
   // Deploy voter
-  const voter = await deploy<Voter>("Voter");
-  await writeOutput(hardhat, voter, "Voter", "Voter.sol");
-  // Deploy bribe factory
-  const bribeFactory = await deploy<BribeFactory>("BribeFactory");
-  await writeOutput(hardhat, bribeFactory, "BribeFactory", "factories/BribeFactory.sol");
-  // Initialize bribe factory, and voter
-  await bribeFactory.initialize(voter.address, deployments.PermissionsRegistry.contractAddress, [...shape.defaultRewardTokens, deployments.WETH.contractAddress, deployments.Reactor.contractAddress]);
-  await voter.initialize(deployments.VotingEscrow.contractAddress, deployments.PairFactory.contractAddress, deployments.GaugeFactory.contractAddress, bribeFactory.address);
-  // Deploy rewards distributor
-  const rewardsDistributor = await deploy<RewardsDistributor>("RewardsDistributor", deployments.VotingEscrow.contractAddress);
-  await writeOutput(hardhat, rewardsDistributor, "RewardsDistributor", "RewardsDistributor.sol");
-  // Deploy minter
-  const minter = await deploy<MinterUpgradeable>("MinterUpgradeable");
-  await writeOutput(hardhat, minter, "MinterUpgradeable", "MinterUpgradeable.sol");
-  // Initialize minter
-  await minter.initialize(voter.address, deployments.VotingEscrow.contractAddress, rewardsDistributor.address);
-  const rct = await getContractWithAddress<Reactor>(deployments.Reactor.contractAddress, "Reactor", "Reactor.sol")
-  // Set RCT minter
-  await rct.setMinter(minter.address);
-  // Mint initial
-  await minter._initialize(shape.initialMintRecipients, shape.initialMintAmounts, BigInt(100_000_000 * 1e18));
+  // const voter = await deploy<Voter>("Voter");
+  // await writeOutput(hardhat, voter, "Voter", "Voter.sol");
+  // // Deploy bribe factory
+  // const bribeFactory = await deploy<BribeFactory>("BribeFactory");
+  // await writeOutput(hardhat, bribeFactory, "BribeFactory", "factories/BribeFactory.sol");
+  // // Initialize bribe factory, and voter
+  // await bribeFactory.initialize(voter.address, deployments.PermissionsRegistry.contractAddress, [
+  //   ...shape.defaultRewardTokens,
+  //   deployments.WETH.contractAddress,
+  //   deployments.Reactor.contractAddress
+  // ]);
+  // await voter.initialize(
+  //   deployments.VotingEscrow.contractAddress,
+  //   deployments.PairFactory.contractAddress,
+  //   deployments.GaugeFactory.contractAddress,
+  //   bribeFactory.address
+  // );
+  // // Deploy rewards distributor
+  // const rewardsDistributor = await deploy<RewardsDistributor>("RewardsDistributor", deployments.VotingEscrow.contractAddress);
+  // await writeOutput(hardhat, rewardsDistributor, "RewardsDistributor", "RewardsDistributor.sol");
+  // // Deploy minter
+  // const minter = await deploy<MinterUpgradeable>("MinterUpgradeable");
+  // await writeOutput(hardhat, minter, "MinterUpgradeable", "MinterUpgradeable.sol");
+  // // Initialize minter
+  // await minter.initialize(voter.address, deployments.VotingEscrow.contractAddress, rewardsDistributor.address);
+  // const rct = await getContractWithAddress<Reactor>(deployments.Reactor.contractAddress, "Reactor", "Reactor.sol");
+  // // Set RCT minter
+  // await rct.setMinter(minter.address);
+  // // Mint initial
+  // await minter._initialize(shape.initialMintRecipients, shape.initialMintAmounts, BigInt(100_000_000 * 1e18));
 
   // ***** Trading *****
   // Deploy trade helper
-  const tradeHelper = await deploy<TradeHelper>("TradeHelper", deployments.PairFactory.contractAddress);
-  await writeOutput(hardhat, tradeHelper, "TradeHelper", "api/TradeHelper.sol");
+  // const tradeHelper = await deploy<TradeHelper>("TradeHelper", deployments.PairFactory.contractAddress);
+  // await writeOutput(hardhat, tradeHelper, "TradeHelper", "api/TradeHelper.sol");
   // Deploy router
-  const router = await deploy<Router>("Router", tradeHelper.address, deployments.WETH.contractAddress);
+  const router = await deploy<Router>("Router", deployments.TradeHelper.contractAddress, shape.wrappedEther as string);
   await writeOutput(hardhat, router, "Router", "Router.sol");
 
   // ***** Other APIs *****
   // Deploy pair helper
-  const pairHelper = await deploy<PairHelper>("PairHelper");
-  await writeOutput(hardhat, pairHelper, "PairHelper", "api/PairHelper.sol");
-  await pairHelper.initialize(voter.address);
+  // const pairHelper = await deploy<PairHelper>("PairHelper");
+  // await writeOutput(hardhat, pairHelper, "PairHelper", "api/PairHelper.sol");
+  // await pairHelper.initialize(deployments.Voter.contractAddress);
   // Deploy reward helper
   const rewardHelper = await deploy<RewardHelper>("RewardHelper");
   await writeOutput(hardhat, rewardHelper, "RewardHelper", "api/RewardHelper.sol");
-  await rewardHelper.initialize(voter.address);
+  await rewardHelper.initialize(deployments.Voter.contractAddress);
   // Deploy veNFT helper
   const veNFTHelper = await deploy<VeNFTHelper>("veNFTHelper");
   await writeOutput(hardhat, veNFTHelper, "veNFTHelper", "api/veNFTHelper.sol");
-  await veNFTHelper.initialize(voter.address, rewardsDistributor.address, pairHelper.address);
+  await veNFTHelper.initialize(deployments.Voter.contractAddress, deployments.RewardsDistributor.contractAddress, deployments.PairHelper.contractAddress);
 }
 
 main()
