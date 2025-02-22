b3b793d5e6483fd14365bc4d7f0f0a05591719a4
Kingsley Victor <javaprodigy56@gmail.com>
2025-02-23 00:34:56 +0100

    deploy coins

--- hardhat.config.js
+++ hardhat.config.js
@@ -65,7 +65,8 @@ const config = {
     },
 
     monadTestnet: {
-      url: "https://10143.rpc.thirdweb.com/f4f634aad888c9e6d43dd22ed5d02158",
+      url: "https://testnet-rpc.monad.xyz",
+      // url: "https://10143.rpc.thirdweb.com/f4f634aad888c9e6d43dd22ed5d02158",
       chainId: 10143,
       accounts: [PRIVATE_KEY],
       gasPrice: "auto",
