5945a208c74f7d3ea306fe71432e6ee7704025e3
Kingsley Victor <javaprodigy56@gmail.com>
2025-03-30 04:29:14 +0100

    deploy freshies

---
+++ contracts/oracle/sources/UniswapV2PriceSource.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.8.0;
+
+import "../PriceSource.sol";
+import "../interfaces/IUniswapV2Factory.sol";
+import "../interfaces/IUniswapRouters.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract UniswapV2PriceSource is PriceSource {
+    IUniswapV2Factory public immutable factory;
+    IUniswapV2Router02 public immutable router02;
+
+    constructor(
+        IUniswapV2Factory _factory,
+        IUniswapV2Router02 _router02,
+        address _usdt,
+        address _usdc,
+        address _weth
+    ) PriceSource("UniswapV2", _usdt, _usdc, _weth) {
+        factory = _factory;
+        router02 = _router02;
+    }
+
+    function _deriveAmountOut(
+        address token0,
+        address token1,
+        uint256 _amountIn
+    ) internal view returns (uint256 amountOut) {
+        if (_amountIn == 0) return 0;
+        if (token0 == token1) return _amountIn;
+
+        address pair = factory.getPair(token0, token1);
+        if (pair == address(0)) return 0;
+
+        address[] memory path = new address[](2);
+        path[0] = token0;
+        path[1] = token1;
+
+        uint256[] memory amounts = router02.getAmountsOut(_amountIn, path);
+        amountOut = amounts[amounts.length - 1];
+    }
+
+    function _getUnitValueInETH(address token) internal view override returns (uint256 amountOut) {
+        uint8 _decimals = ERC20(token).decimals();
+        uint256 _amountIn = 1 * 10 ** _decimals;
+        amountOut = _deriveAmountOut(token, weth, _amountIn);
+    }
+
+    function _getUnitValueInUSDC(address token) internal view override returns (uint256) {
+        uint256 _valueInETH = _getUnitValueInETH(token);
+        uint256 _ethUSDCAmountOut = _deriveAmountOut(weth, usdc, _valueInETH);
+
+        if (_ethUSDCAmountOut > 0) {
+            return _ethUSDCAmountOut;
+        } else {
+            uint8 _tokenDecimals = ERC20(token).decimals();
+            uint256 _amountIn = 1 * 10 ** _tokenDecimals;
+            uint256 amountOut = _deriveAmountOut(token, usdc, _amountIn);
+            return amountOut;
+        }
+    }
+
+    function _getUnitValueInUSDT(address token) internal view override returns (uint256) {
+        uint256 _valueInETH = _getUnitValueInETH(token);
+        uint256 _ethUSDTAmountOut = _deriveAmountOut(weth, usdt, _valueInETH);
+
+        if (_ethUSDTAmountOut > 0) {
+            return _ethUSDTAmountOut;
+        } else {
+            uint8 _tokenDecimals = ERC20(token).decimals();
+            uint256 _amountIn = 1 * 10 ** _tokenDecimals;
+            uint256 amountOut = _deriveAmountOut(token, usdt, _amountIn);
+            return amountOut;
+        }
+    }
+}
